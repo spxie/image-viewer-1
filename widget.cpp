7a906762f202b14552f84024d81e4e694cef8769
zhibiao <243027820@qq.com>
2017-07-26 15:46:26 +0800

    Init project

---
+++ widget.cpp
@@ -0,0 +1,15 @@
+#include "widget.h"
+#include "ui_widget.h"
+
+Widget::Widget(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+    this->setWindowTitle("image viewre");
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
